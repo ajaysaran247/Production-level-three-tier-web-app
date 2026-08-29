a8d8395130930a66785a964b36474c034acef7d8
ajaysaran247 <panneerselvampanneersudha@gmail.com>
2026-08-29 13:57:16 +0000

    FF

--- application/frontend/src/api.js
+++ application/frontend/src/api.js
@@ -1,6 +1,3 @@
-Updated upstream
-const API="http://three-tier-alb-373220144.us-east-1.elb.amazonaws.com";
-const API_URL ="http://three-tier-alb-58519730.us-east-1.elb.amazonaws.com";
-Stashed changes
+const API_URL = "http://three-tier-alb-373220144.us-east-1.elb.amazonaws.com";
 
 export default API_URL;
