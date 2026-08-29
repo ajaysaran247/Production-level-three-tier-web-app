c29cc5e4801ae935fe5342062245b6484deb9872
Saran P S <panneerselvampanneersudha@gmail.com>
2026-08-29 19:04:20 +0530

    Update App.js

--- application/frontend/src/App.js
+++ application/frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div>
-      <h1>AWS Three Tier Application</h1>
+      <h1>AWS Three Tier Application This is kamalesh website</h1>
 
       <h2>{data.message}</h2>
 
