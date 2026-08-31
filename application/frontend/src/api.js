a879cec7c866578e5d209dd6274323854cab8f91
Saran P S <panneerselvampanneersudha@gmail.com>
2026-08-31 19:40:55 +0530

    Update api.js

--- application/frontend/src/api.js
+++ application/frontend/src/api.js
@@ -1,3 +1,3 @@
-const API_URL = "http://three-tier-alb-373220144.us-east-1.elb.amazonaws.com";
+const API_URL = "http://three-tier-alb-1022638616.us-east-1.elb.amazonaws.com";
 
 export default API_URL;
